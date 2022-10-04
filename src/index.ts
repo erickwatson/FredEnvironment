54a0bdeb6a48c2bd2cc015199169a2056e6a8a03
Brendan Allan <brendonovich@outlook.com>
2022-10-04 13:18:18 +0800

    use getPropertyListItems

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { OBS } from "@sceneify/core";
 import dotenv from "dotenv";
 
 import { setupMixItUp } from "./mixitup";
-import { cameraScene, decklinkCamera } from "./scenes/camera";
+import { decklinkCamera } from "./scenes/camera";
 import { mainScene } from "./scenes/main";
 import { programScene } from "./scenes/program";
 
@@ -13,20 +13,20 @@ dotenv.config();
 
 async function main() {
   const obs = new OBS();
-  
+
   // Connect Websocket to OBS
   await obs.connect("ws://localhost:4455", process.env.OBS_PASSWORD);
-  
+
   // Create "Main Scene"
   await mainScene.create(obs);
-  
+
   // Ask for the Canvas Size
   await setBGSize();
-  
+
   // Link to existing scene "Main Scene"
   await programScene.link(obs);
-  
-  // Create a new 
+
+  // Create a new
   await programScene.createItem("mainScene", {
     source: mainScene,
     enabled: false,
@@ -51,40 +51,26 @@ async function setBGSize() {
 
 async function configureCamera() {
   // fetch input list directly from OBS
-  const { propertyItems: deviceHashes } = await cameraScene.obs.call(
-    "GetInputPropertiesListPropertyItems",
-    {
-      inputName: decklinkCamera.name,
-      propertyName: "device_hash",
-    }
-  );
+  const deviceHashes = await decklinkCamera.getPropertyListItems("device_hash");
 
   // find input with matching name
   const deviceHash = deviceHashes.find(
-    (i) => i.itemName === "DeckLink Quad HDMI Recorder (1)"
+    (i) => i.name === "DeckLink Quad HDMI Recorder (1)"
   );
 
   // if input is found, update settings
-  if (deviceHash) {
+  if (deviceHash)
     await decklinkCamera.setSettings({
-      device_hash: deviceHash.itemValue,
+      device_hash: deviceHash.value,
     });
-  }
 
-  const { propertyItems: modeIds } = await cameraScene.obs.call(
-    "GetInputPropertiesListPropertyItems",
-    {
-      inputName: decklinkCamera.name,
-      propertyName: "mode_id",
-    }
-  );
+  const modeIds = await decklinkCamera.getPropertyListItems("mode_id");
 
   // find input with matching name
-  const modeID = modeIds.find((i) => i.itemName === "1080p60");
+  const modeId = modeIds.find((i) => i.name === "1080p60");
 
-  if (modeID) {
+  if (modeId)
     await decklinkCamera.setSettings({
-      mode_id: modeID.itemValue as any as number, // types annoying
+      mode_id: modeId.value,
     });
-  }
 }
